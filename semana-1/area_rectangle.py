59ae8597ceef0191baa4086b252088b4545c718d
ferrerlantigua01 <dopeybbs@gmail.com>
2026-01-26 10:44:14 -0400

    Agregando comentarios.

--- semana-1/area_rectangle.py
+++ semana-1/area_rectangle.py
@@ -2,9 +2,12 @@
 Calcula el área de un rectángulo.
 
 """
+# Se le solicita al usuario qe ingrese los datos por teclado.
 base: float = float(input("Ingresa la base: "))
 height: float = float(input("Ingresa la altura: "))
 
+# Formula.
 area_rectangle = base * height
 
+# Mostramos por pantalla el resultado.
 print(f"El área del rectángulo es: {area_rectangle}")
